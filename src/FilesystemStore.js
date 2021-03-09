2b0d8a8aac18f49ff42ace5cfb9fd05cf83d9cef
Матвей Кирик <ikirik59@gmail.com>
2021-03-09 21:22:12 +0200

    Add FilesystemStore stub

---
+++ src/FilesystemStore.js
@@ -0,0 +1,25 @@
+class FilesystemStore {
+    save(data) {
+
+    }
+
+    getById(chatId, messageId) {
+
+    }
+
+    findLast(chatId, filter, limit) {
+
+    }
+
+    update(chatId, messageId, tags, data) {
+
+    }
+
+    deleteById(chatId, messageId) {
+
+    }
+
+    deleteLast(chatId, filter, limit) {
+        
+    }
+}
