cef3318cdbbdfd6c9cbce6470fc9f862aeae968f
Матвей Кирик <ikirik59@gmail.com>
2021-03-10 19:58:07 +0200

    Add directory initialization

--- src/FilesystemStore.js
+++ src/FilesystemStore.js
@@ -1,4 +1,24 @@
+const __fs = require('fs').promises;
+const __path = require('path');
+
 class FilesystemStore {
+
+    constructor(basePath, tags) {
+        return (async () => {
+            await this.setDirectory(basePath);
+
+            return this;
+        });
+    }
+
+    async setDirectory(basePath) {
+        if(typeof basePath === 'string') this.basePath = basePath;
+        else throw new Error('Base path should be a string.');
+
+        let dataPath = __path.join(path, 'data');
+        await __fs.mkdir(dataPath, {recursive: true});
+    }
+
     save(data) {
 
     }
@@ -20,6 +40,6 @@ class FilesystemStore {
     }
 
     deleteLast(chatId, filter, limit) {
-        
+
     }
 }
