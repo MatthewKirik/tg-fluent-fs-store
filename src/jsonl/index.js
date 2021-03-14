310e3069fbe51a88e490c7832418a26ef9aade78
Матвей Кирик <ikirik59@gmail.com>
2021-03-14 19:03:50 +0200

    Add and test jsonl readFromEnd

--- src/jsonl/index.js
+++ src/jsonl/index.js
@@ -1,20 +1,62 @@
 const __fs = require('fs').promises;
 const __path = require('path');
 
-const serialize = async (obj) => JSON.stringify(obj) + '\n';
+const delimeter = '\n';
+
+const serialize = async (obj) => JSON.stringify(obj) + delimeter;
+const deserialize = async (str) => JSON.parse(str);
 
 const write = async (path, ...objects) => {
-    fileHandle = await __fs.open(path, 'a');
-    if(fileHandle === undefined) throw new Error('Could not open the file!'); 
-
-    for (const obj of objects) {
-        string = await serialize(obj);
-        await fileHandle.write(string);
+    const fileHandle = await __fs.open(path, 'a');
+    if (fileHandle === undefined) throw new Error('Could not open the file!');
+    try {
+        for (const obj of objects) {
+            string = await serialize(obj);
+            await fileHandle.write(string);
+        }
     }
+    finally {
+        await fileHandle.close();
+    }
+}
 
-    await fileHandle.close();
+const readFromEnd = async function* (path, bufferSize = 1024) {
+    const fileHandle = await __fs.open(path, 'r');
+
+    try {
+        if (fileHandle === undefined) throw new Error('Could not open the file!');
+        const stats = await fileHandle.stat();
+
+        const buffer = new Buffer.alloc(bufferSize);
+        let rest = '';
+        let pos = stats.size;
+        let length;
+        do {
+            pos = pos - bufferSize;
+            length = (pos < 0) ? bufferSize + pos : bufferSize;
+            const readingResult = await fileHandle.read(buffer, 0, length, Math.max(pos, 0));
+
+            const string = buffer.toString("utf-8", 0, readingResult.bytesRead) + rest;
+            const strings = string.split(delimeter);
+
+            if (pos > 0) rest = strings.shift();
+            else rest = '';
+
+            for (let i = strings.length; i >= 0; i--) {
+                try {
+                    const obj = await deserialize(strings[i]);
+                    yield obj;
+                } catch { };
+            }
+        } while (pos > 0);
+
+    }
+    finally {
+        await fileHandle.close();
+    }
 }
 
 module.exports = {
     write,
+    readFromEnd
 };
--- src/jsonl/index.test.js
+++ src/jsonl/index.test.js
@@ -1,8 +1,9 @@
 const jsonl = require('./index');
 const __path = require('path');
 const __fs = require('fs').promises;
+const __assert = require('assert').strict;
 
-const testBasePath = '../../test/test-files';
+const testBasePath = __dirname + '../../../test/test-files';
 
 const clear = async (path) => {
     try {
@@ -17,19 +18,35 @@ const clear = async (path) => {
     const object = { 
         string : "hello",
         number: 1, 
-        date: new Date()
+        tags: ['admin', 'service']
     };
     await jsonl.write(path, object);
-})().then(() => {
+    const objects = jsonl.readFromEnd(path);
+    const readObject = (await objects.next()).value;
+    __assert.deepStrictEqual(object, readObject, 'Read/write single object');
+})()
+.then(() => {
     (async () => {
         const path = __path.join(testBasePath, 'jsonl-test.jsonl');
         await clear(path);
     
         const object = { 
+            index : 0,
             string : "hello",
             number: 1, 
-            date: new Date()
+            tags: ['admin', 'service']
         };
-        await jsonl.write(path, object, object, object);
+        const arr = [];
+        for (let i = 0; i < 10000; i++) {
+            arr.push({ ...object, index: i});
+        }
+        await jsonl.write(path, ...arr);
+        
+        const readArr = [];
+        for await(let obj of jsonl.readFromEnd(path)) {
+            readArr.unshift(obj);
+        }
+
+        __assert.deepStrictEqual(arr, readArr, 'Read/write 10000 objects');
     })()
 });
